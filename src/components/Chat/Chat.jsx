aa74938b35810190ba36d07f6902a283ec92e4a4
Lucas <lucasspindolafernandes196@hotmail.com>
2023-05-25 22:20:00 -0300

    first commit

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+export const Chat = () => {
+  return (
+    <main>
+      <h1>Chat</h1>;
+      <input type="text" placeholder="Nova mensagem" />
+      <button>Enviar</button>
+    </main>
+  );
+};
+
+export default Chat;
